6bfa1262485d9ef7ada710c23e2e7b84ff1a6934
Kris Kurniawan <kris7kurniawan@gmail.com>
2023-12-09 14:42:47 +0900

    Update Profile

--- common/constant/index.ts
+++ common/constant/index.ts
@@ -2,7 +2,7 @@ export const PLACEHOLDER_URL = 'https://res.cloudinary.com/dvlbwm8c1/image/uploa
 export const DEVTO_PROFILE = 'https://www.linkedin.com/in/kriskurniawan/'
 export const PAGESPEED_URL = 'https://pagespeed.web.dev/'
 export const PAGESPEED_CATEGORIES = '&category=accessibility&category=performance&category=best-practices&category=seo'
-export const PROFILE_URL = 'https://res.cloudinary.com/dgaf5rmnv/image/upload/c_crop,g_auto,h_500,w_500/dotst8mkwvrlylku0zs8.jpg'
+export const PROFILE_URL = 'https://res.cloudinary.com/dgaf5rmnv/image/upload/v1702099999/k0oi9bbtgy7yqm8i8ptz.jpg'
 export const DEVTO_BLOG_API = 'https://dev.to/api/articles?username=codebayu'
 export const BACKDROP_IMAGE = 'https://res.cloudinary.com/dvlbwm8c1/image/upload/v1696122641/codebayu/hxbt52djgplqqzbrux93.webp'
 
